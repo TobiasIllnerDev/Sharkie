eeb77c37387c096f9018526124bdfb3b43884432
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-06-25 17:10:00 +0200

    feat: make a background

--- classes/world.class.js
+++ classes/world.class.js
@@ -24,6 +24,7 @@ class World {
     }
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        
         this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character)
         this.addObjectsToMap(this.lights)
