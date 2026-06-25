022d0702fc3ab0510c393416cc727bede924e2d3
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-06-25 10:08:01 +0200

    style: rename folders

--- classes/character.class.js
+++ classes/character.class.js
@@ -2,6 +2,6 @@ class Character extends MovableObject {
 
 
     constructor() {
-        super().loadImage('../assets/img/1.Sharkie/1.IDLE/1.png')
+        super().loadImage('../assets/img/Sharkie/1.IDLE/1.png')
     }
 }
--- classes/world.class.js
+++ classes/world.class.js
@@ -5,15 +5,26 @@ class World {
         new JellyFish(),
         new JellyFish(),
     ];
+    canvas;
     ctx;
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
-
-
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width);
+
+        this.enemies.forEach(enemy => {
+            (enemy.img, enemy.x, enemy.y, enemy.height, enemy.width);
+        });
+
+
+        let self = this;
+        requestAnimationFrame(function(){
+            self.draw();
+        });
     }
 }
