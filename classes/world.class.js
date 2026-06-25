09aa7c9d4926e6f09eda90405e49fd7562eccfe7
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-06-25 07:56:33 +0200

    feat: great World class

---
+++ classes/world.class.js
@@ -0,0 +1,13 @@
+class World {
+    character = new Character();
+    enemies = [
+        new JellyFish(),
+        new JellyFish(),
+        new JellyFish(),
+    ];
+
+
+    draw() {
+        
+    }
+}
