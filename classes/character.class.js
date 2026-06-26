dfcb303f9a02797bda093d55b2958cb96a6e95a7
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-06-26 16:13:16 +0200

    feat: form from objects

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,5 +1,6 @@
 class Character extends MovableObject {
 
+    height = 300;
 
     constructor() {
         super().loadImage('../assets/img/Sharkie/1.IDLE/1.png')
--- classes/jelly-fish.class.js
+++ classes/jelly-fish.class.js
@@ -4,7 +4,7 @@ class JellyFish extends MovableObject {
     constructor() {
         super().loadImage('../assets/img/Enemy/JellyFish/Regular/Purpel1.png')
         this.x = 200 + Math.random() * 500;
-        this.height = 100;
-        this.width = 100;
+        this.height = 70;
+        this.width = 70;
     }
 }
