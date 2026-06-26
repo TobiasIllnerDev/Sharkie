4584b2f2340d7e247bb412353d0401c81f064aac
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-06-26 16:47:25 +0200

    feat: animate sharkie

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,16 +1,30 @@
 class Character extends MovableObject {
-
-    height = 300;
-
-    constructor() {
-        super().loadImage('../assets/img/Sharkie/1.IDLE/1.png')
-        this.loadImages([
+    IMAGES_WALKING = [
             '../assets/img/Sharkie/1.IDLE/1.png',
             '../assets/img/Sharkie/1.IDLE/2.png',
             '../assets/img/Sharkie/1.IDLE/3.png',
             '../assets/img/Sharkie/1.IDLE/4.png',
             '../assets/img/Sharkie/1.IDLE/5.png',
             '../assets/img/Sharkie/1.IDLE/6.png'
-        ]);
+        ];
+    currentImage = 0;
+    height = 300;
+
+    constructor() {
+        super().loadImage('../assets/img/Sharkie/1.IDLE/1.png')
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.animate();
+    }
+
+    animate() {
+
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage ++;
+        }, 100);
+        
     }
 }
