13697336b5fd0726d8f74fa42940e6c31ef28976
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-06-26 16:34:15 +0200

    feat: added more images

--- classes/character.class.js
+++ classes/character.class.js
@@ -4,5 +4,13 @@ class Character extends MovableObject {
 
     constructor() {
         super().loadImage('../assets/img/Sharkie/1.IDLE/1.png')
+        this.loadImages([
+            '../assets/img/Sharkie/1.IDLE/1.png',
+            '../assets/img/Sharkie/1.IDLE/2.png',
+            '../assets/img/Sharkie/1.IDLE/3.png',
+            '../assets/img/Sharkie/1.IDLE/4.png',
+            '../assets/img/Sharkie/1.IDLE/5.png',
+            '../assets/img/Sharkie/1.IDLE/6.png'
+        ]);
     }
 }
--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -4,10 +4,19 @@ class MovableObject {
     height = 200;
     width = 300;
     img;
+    imageCache = [];
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        })
     }
 
     moveRight() {
