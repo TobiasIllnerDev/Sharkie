28b30df89b6edf442388f3b03ce10f78f8e0c7c9
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-06-27 12:57:00 +0200

    feat: make world bigger

--- classes/world.class.js
+++ classes/world.class.js
@@ -9,10 +9,31 @@ class World {
         new Light()
     ]
     backgroundObjects =  [
-        new BackgroundObject('../assets/img/Background/Layers/5. Water/D1.png', 0 ),
+        new BackgroundObject('../assets/img/Background/Layers/5. Water/D2.png', -719),
+        new BackgroundObject('../assets/img/Background/Layers/3.Fondo 1/L2.png', -719),
+        new BackgroundObject('../assets/img/Background/Layers/4.Fondo 2/L2.png', -719),
+        new BackgroundObject('../assets/img/Background/Layers/2. Floor/D2.png', -719),
+
+        new BackgroundObject('../assets/img/Background/Layers/5. Water/D1.png', 0),
         new BackgroundObject('../assets/img/Background/Layers/3.Fondo 1/L1.png', 0),
         new BackgroundObject('../assets/img/Background/Layers/4.Fondo 2/L1.png', 0),
-        new BackgroundObject('../assets/img/Background/Layers/2. Floor/D1.png', 0),   
+        new BackgroundObject('../assets/img/Background/Layers/2. Floor/D1.png', 0), 
+
+        new BackgroundObject('../assets/img/Background/Layers/5. Water/D2.png', 719),
+        new BackgroundObject('../assets/img/Background/Layers/3.Fondo 1/L2.png', 719),
+        new BackgroundObject('../assets/img/Background/Layers/4.Fondo 2/L2.png', 719),
+        new BackgroundObject('../assets/img/Background/Layers/2. Floor/D2.png', 719),
+        
+        new BackgroundObject('../assets/img/Background/Layers/5. Water/D1.png', 719*2),
+        new BackgroundObject('../assets/img/Background/Layers/3.Fondo 1/L1.png', 719*2),
+        new BackgroundObject('../assets/img/Background/Layers/4.Fondo 2/L1.png', 719*2),
+        new BackgroundObject('../assets/img/Background/Layers/2. Floor/D1.png', 719*2),
+
+        new BackgroundObject('../assets/img/Background/Layers/5. Water/D2.png', 719*3),
+        new BackgroundObject('../assets/img/Background/Layers/3.Fondo 1/L2.png', 719*3),
+        new BackgroundObject('../assets/img/Background/Layers/4.Fondo 2/L2.png', 719*3),
+        new BackgroundObject('../assets/img/Background/Layers/2. Floor/D2.png', 719*3),
+        
     ]
     canvas;
     keyboard;
