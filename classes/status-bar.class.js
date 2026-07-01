45cd963ac511d3608f5a322ac2b7aefaa6178e23
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-07-01 22:42:14 +0200

    feat: make logic for statusbar

--- classes/status-bar.class.js
+++ classes/status-bar.class.js
@@ -1,3 +1,32 @@
-class StatusBar{
-    
+class StatusBar extends DrawableObject{
+
+    IMAGES = Array.from({length: 6}, (_, i) => `../assets/img/Marcadores/green/Life/${i+1}_Lifebar.png`)
+    percentage = 100;
+
+    constructor() {
+        this.loadImages(this.IMAGES);
+    }
+
+    setPercentage(percentage) {
+        this.percentage = percentage;
+        let path = this.IMAGES[this.resolveImageIndex()];
+        this.img = this.imageCache[path]   
+    }
+
+    resolveImageIndex(){
+
+         if(this.percentage === 100) {
+            return 5;
+        } else if (this.percentage > 80) {
+            return 4;
+        }  else if (this.percentage > 60) {
+            return 3;
+        }  else if (this.percentage > 40) {
+            return 2;
+        }  else if (this.percentage > 20) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
