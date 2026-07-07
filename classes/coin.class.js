9c8b14969a3923ada9d679f72c6a4046b1cddcf9
Tobias Illner <140509788+BlueShadow07@users.noreply.github.com>
2026-07-07 14:45:52 +0200

    feat: bottle and coin bug fix animated

--- classes/coin.class.js
+++ classes/coin.class.js
@@ -4,10 +4,4 @@ class Coin extends CollectibleObject {
         const IMAGES = Array.from({length: 4}, (_, i) => `../assets/img/Marcadores/1. Coins/${i+1}.png`);
         super(IMAGES, x, y, 40, 40);
     }
-
-    animate() {  
-        setInterval(() => {
-            this.playAnimation(this.IMAGES);
-        }, 200);
-    }
 }
